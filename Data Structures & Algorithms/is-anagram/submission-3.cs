7a27aaaa92e9cf3e6f7c05ae3a142093190a8c14
Mohammed Inaam <149177211+DMInaam@users.noreply.github.com>
2026-05-14 12:20:59 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) return false;
+        int[] freq = new int[26];
+        foreach(char c in s){
+            freq[c - 'a']++;
+        }
+        foreach(char c in t){
+            freq[c-'a']--;
+            if(freq[c-'a']<0) return false;
+        }
+        return true;
+    }
+}
